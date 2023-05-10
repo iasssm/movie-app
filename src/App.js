8fb4163df41b90ebeafc713fd9a86083d5a51052
iasmina <iasmina.calugar16@gmail.com>
2023-05-10 19:39:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard.jsx';
 //5c93dd09
 
-// const movie1 = {
-//   Title: 'Spiderman the Verse',
-//   Year: '2019–',
-//   imdbID: 'tt12122034',
-//   Type: 'series',
-//   Poster:
-//     'https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg',
-// };
+const movie1 = {
+  Title: 'Spiderman the Verse',
+  Year: '2019',
+  imdbID: 'tt12122034',
+  Type: 'series',
+  Poster:
+    'https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg',
+};
 const API_URL = 'http://www.omdbapi.com?apikey=5c93dd09';
 const App = () => {
   const [movies, setMovies] = useState([]);
